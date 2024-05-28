19c68963f425ba4f8464cdcb5c31c78a78218c07
richahoipen <minhyemen@gmail.com>
2024-05-28 08:39:36 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink, Routes } from 'react-router-dom';
+import LoginScreen from './loginScreen/LoginScreen';
+//import ShoppingCart from "./clothScreen/ShoppingCart";
+import StoreScreen from "./store/StoreScreen";
+import ClothingScreen from "./clothing/ClothingScreen";
+//mport Car from "./car/Car";
+
+import Product from "./car/Product";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <NavLink className="navbar-brand" exact to="/" activeClassName="active">Trang chủ</NavLink>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/store" activeClassName="active">Thông tin cửa
+                                    hàng</NavLink>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/clothing" activeClassName="active">Thông tin quần
+                                    áo</NavLink>
+                            </li>
+                        </ul>
+
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/product" activeClassName="active">Thông tin Product</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<LoginScreen/>}/>
+                <Route path="/store" element={<StoreScreen/>}/>
+                <Route path="/clothing" element={<ClothingScreen/>}/>
+                <Route path="/product" element={<Product/>}/>
+
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
+//<Route path="/product" element={<Product/>}/>
+
+//<LoginScreen/>
+//
+//<LoginScreen/>
+// <ShoppingCartTable/>
+//<ShoppingCart/>
